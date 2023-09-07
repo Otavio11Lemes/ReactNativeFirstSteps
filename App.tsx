7332bb6bf1de0fb74ba760d95d23b29cb166755f
Otavio Lemes <otavio11lemes@outlook.com>
2023-09-07 15:21:45 -0300

    Base do app

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text style = {{
+        fontSize : 30,
+        color: 'grey',
+        backgroundColor: 'blue'
+      }}>Hello world!</Text>
+      <Text>Jesus is the top da parada </Text>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
